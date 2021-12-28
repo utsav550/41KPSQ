a178b9f48b73a9a13d93836010303508070cc0b2
UTSAV <77149569+utsav550@users.noreply.github.com>
2021-12-28 19:49:48 +1000

    login || Event

--- 41kpsq/routes/web.php
+++ 41kpsq/routes/web.php
@@ -1,5 +1,7 @@
 <?php
-
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\EventsController;
+use App\Models\Admin;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -15,4 +17,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('admin',[AdminController::class,'index']);
+Route::post('admin/auth',[AdminController::class,'auth'])->name('admin.auth');
+
+Route::group(['middleware'=>'admin_auth'],function(){
+    Route::get('admin/dashboard',[AdminController::class,'dashboard']);
+    Route::post('admin/events/add',[EventsController::class,'add'])->name('event.add');
+    Route::get('admin/events',[EventsController::class,'index']); 
+    Route::get('admin/events/add_event',[EventsController::class,'add_event']);
+    Route::get('admin/events/add_event/{id}',[EventsController::class,'add_event'])->name('event.manage');
+    Route::get('admin/events/delete/{id}',[EventsController::class,'delete']);
+
+
+    Route::get('admin/logout', function () {
+        session()->forget('ADMIN_LOGIN');
+        session()->forget('ADMIN_ID');
+        return redirect('admin');    
+        return view('welcome');
+    });
+    
+    
 });
