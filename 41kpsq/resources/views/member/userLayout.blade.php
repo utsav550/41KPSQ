ef49784515851b1954ccab9eebbd77d705d592c5
UTSAV <77149569+utsav550@users.noreply.github.com>
2022-01-16 20:31:16 +1000

    fix nav

--- 41kpsq/resources/views/member/userLayout.blade.php
+++ 41kpsq/resources/views/member/userLayout.blade.php
@@ -403,15 +403,15 @@
         </div>
     </div>
 
-    <!-- Bootstrap core JavaScript-->
     <script src="{{asset('main_asset/vendor/jquery/jquery.min.js')}}"></script>
     <script src="{{asset('main_asset/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
-
-    <!-- Core plugin JavaScript-->
     <script src="{{asset('main_asset/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
-
-    <!-- Custom scripts for all pages-->
     <script src="{{asset('main_asset/js/sb-admin-2.min.js')}}"></script>
+    <script src="{{asset('main_asset/vendor/chart.js/Chart.min.js')}}"></script>
+    <script src="{{asset('main_asset/js/demo/chart-area-demo.js')}}"></script>
+    <script src="{{asset('main_asset/js/demo/chart-pie-demo.js')}}"></script> 
+
+  
 
 </body>
 
